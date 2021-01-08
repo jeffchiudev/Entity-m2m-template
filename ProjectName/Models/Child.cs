806da566ef276bb3266c506098821108bcbc2479
Jeff Chiu <jwchiu82@gmail.com>
2021-01-08 09:06:25 -0800

    add boilerplate template for child/parent db .cs file

---
+++ ProjectName/Models/Child.cs
@@ -0,0 +1,17 @@
+using System.Collections.Generic;
+
+namespace ProjectName.Models
+{
+    public class Child
+    {
+        public Child()
+        {
+            this.Parents = new HashSet<ParentChild>();
+        }
+
+        public int ChildId { get; set; }
+        public string Description { get; set; }
+
+        public ICollection<ParentChild> Parents { get;}
+    }
+}
